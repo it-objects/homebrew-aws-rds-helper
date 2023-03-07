ac90a55332e7f7e96631c6196fd46e585561ae82
GuidoSelinger <123949809+GuidoSelinger@users.noreply.github.com>
2023-03-07 12:56:57 +0100

    Add files via upload

---
+++ aws-rds-helper.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AwsRdsHelper < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/it-objects/aws-rds-helper/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "432b48abe05429bbde29408c9cafddff5b693b587872a91c18a9faea59a29e59"
+  license ""
+
+  def install
+      bin.install "aws-rds-helper"
+    
+  end
+
+  
+  end
+end
